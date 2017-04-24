7e4065ff2b14f888670c41119b13929224f48fb5
Taras Ignashchenko <tignashchenko@gmail.com>
2017-04-25 02:27:57 +0300

    implement getLetterRange()

---
+++ client/js/test/array-util.test.js
@@ -0,0 +1,34 @@
+const { getRange,
+        getLetterRange } = require('../array-util');
+
+describe('array-util', () => {
+
+  describe('getRange()', () => {
+    it('produces a valid range starting with 0', () => {
+      expect(getRange(0, 5)).toEqual([0, 1, 2, 3, 4, 5]);
+    });
+
+    it('produces a valid range starting with 1', () => {
+      expect(getRange(1, 5)).toEqual([1, 2, 3, 4, 5]);
+    });
+
+    it('produces a valid negative range', () => {
+      expect(getRange(-10, -7)).toEqual([-10, -9, -8, -7]);
+    });
+  });
+
+  describe('getLetterRange()', () => {
+    it('produces a valid single letter range', () => {
+      expect(getLetterRange('Q', 1)).toEqual(['Q']);
+    });
+
+    it('produces a valid letter range starting from A', () => {
+      expect(getLetterRange('A', 5)).toEqual(['A', 'B', 'C', 'D', 'E']);
+    });
+
+    it('produces a valid letter range starting from B', () => {
+      expect(getLetterRange('B', 5)).toEqual(['B', 'C', 'D', 'E', 'F']);
+    });
+  });
+
+});
