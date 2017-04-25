502711f36a8cb956cebe154f810927607f82928e
Taras Ignashchenko <tignashchenko@gmail.com>
2017-04-25 23:53:23 +0300

    add data rows

--- client/js/table-view.js
+++ client/js/table-view.js
@@ -1,5 +1,5 @@
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTR, createTH, createTD } = require('./dom-util');
 
 class TableView {
   constructor(model) {
@@ -13,10 +13,12 @@ class TableView {
 
   initDomReferences() {
     this.headerRowEl = document.querySelector('THEAD TR');
+    this.sheetBodyEl = document.querySelector('TBODY');
   }
 
   renderTable() {
     this.renderTableHeader();
+    this.renderTableBody();
   }
 
   renderTableHeader() {
@@ -24,6 +26,22 @@ class TableView {
     getLetterRange('A', this.model.numCols)
       .map(colLabel => createTH(colLabel))
       .forEach(th => this.headerRowEl.appendChild(th));
+  }
+
+  renderTableBody() {
+    const fragment = document.createDocumentFragment();
+    for(let row = 0; row < this.model.numRows; row++) {
+      const tr = createTR();
+      for(let col = 0; col < this.model.numCols; col++) {
+        const position = {col: col, row: row};
+        const value = this.model.getValue(position);
+        const td = createTD(value);
+        tr.appendChild(td);
+      }
+      fragment.appendChild(tr);
+    }
+    removeChildren(this.sheetBodyEl);
+    this.sheetBodyEl.appendChild(fragment);
   }
 }
 
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -77,7 +77,7 @@ module.exports = TableModel;
 
 },{}],5:[function(require,module,exports){
 const { getLetterRange } = require('./array-util');
-const { removeChildren, createTH } = require('./dom-util');
+const { removeChildren, createTR, createTH, createTD } = require('./dom-util');
 
 class TableView {
   constructor(model) {
@@ -91,10 +91,12 @@ class TableView {
 
   initDomReferences() {
     this.headerRowEl = document.querySelector('THEAD TR');
+    this.sheetBodyEl = document.querySelector('TBODY');
   }
 
   renderTable() {
     this.renderTableHeader();
+    this.renderTableBody();
   }
 
   renderTableHeader() {
@@ -102,6 +104,22 @@ class TableView {
     getLetterRange('A', this.model.numCols)
       .map(colLabel => createTH(colLabel))
       .forEach(th => this.headerRowEl.appendChild(th));
+  }
+
+  renderTableBody() {
+    const fragment = document.createDocumentFragment();
+    for(let row = 0; row < this.model.numRows; row++) {
+      const tr = createTR();
+      for(let col = 0; col < this.model.numCols; col++) {
+        const position = {col: col, row: row};
+        const value = this.model.getValue(position);
+        const td = createTD(value);
+        tr.appendChild(td);
+      }
+      fragment.appendChild(tr);
+    }
+    removeChildren(this.sheetBodyEl);
+    this.sheetBodyEl.appendChild(fragment);
   }
 }
 
